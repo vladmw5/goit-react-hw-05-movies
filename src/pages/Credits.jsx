7c1a40e28a02d8b2811f94b558a9745c911cb7c5
Vladyslav Avidzba <wasssd1@gmail.com>
2022-07-11 23:25:11 +0300

    done-successfully

--- src/pages/Credits.jsx
+++ src/pages/Credits.jsx
@@ -1,5 +1,53 @@
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+
+import api from 'service/movieAPI';
+import Loader from 'components/Loader';
+
+const POSTER_PATH = 'https://image.tmdb.org/t/p/w500';
+
 const Credits = () => {
-  return ``;
+  const [cast, setCast] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const { movieId } = useParams();
+
+  useEffect(() => {
+    setIsLoading(true);
+    api
+      .fetchMovieCredits(movieId)
+      .then(setCast)
+      .catch(console.error)
+      .finally(setIsLoading(false));
+  }, [movieId]);
+
+  return (
+    <section>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <ul>
+          {cast?.cast.map(({ profile_path, name, character, id }) => (
+            <li key={id}>
+              {profile_path ? (
+                <img
+                  width="100"
+                  src={`${POSTER_PATH}${profile_path}`}
+                  alt={name}
+                />
+              ) : (
+                <p>This actor has no image</p>
+              )}
+              <p>{name}</p>
+              <p>
+                Known as: <span>{character}</span>
+              </p>
+              <br />
+            </li>
+          ))}
+        </ul>
+      )}
+    </section>
+  );
 };
 
 export default Credits;
--- src/pages/Reviews.jsx
+++ src/pages/Reviews.jsx
@@ -1,5 +1,39 @@
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+
+import api from 'service/movieAPI';
+import Loader from 'components/Loader';
+
 const Reviews = () => {
-  return ``;
+  const [reviews, setReviews] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const { movieId } = useParams();
+
+  useEffect(() => {
+    setIsLoading(true);
+    api
+      .fetchMovieReviews(movieId)
+      .then(({ results }) => setReviews(results))
+      .catch(console.error)
+      .finally(() => setIsLoading(false));
+  }, [movieId]);
+
+  return (
+    <section>
+      {isLoading && <Loader />}
+      {reviews && !reviews?.length && <h3>This film has no reviews</h3>}
+      <ul>
+        {reviews?.map(({ author, content, id }) => (
+          <li key={id}>
+            <h3>
+              Author: <span>{author}</span>
+            </h3>
+            <p>{content}</p>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
 };
 
 export default Reviews;
